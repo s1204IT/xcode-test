62d3e79cd094f1f9ddf622852219f6eaf27d254f
Syuugo <pub@s1204.me>
2025-05-09 19:33:42 +0900

    Initial commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Syuugo on 2025/05/09.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
